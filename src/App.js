ccaa820ffdfb0c3f9f7f594eb0d70ea4b3d6c107
devtwins0 <devtwins0@gmail.com>
2022-01-24 21:45:10 +0100

    Update Packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
 import './App.css';
 import Home from './Views/Home.view';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <Home />
     </div>
   );
-}
+};
 
 export default App;
